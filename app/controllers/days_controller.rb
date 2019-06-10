2e20769306f50b76356a99994ce0468fe2155e7b
nerdinand <nerdinand@nerdinand.com>
2019-06-10 23:07:38 +0200

    Add crud actions for days

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,41 @@
+class DaysController < ApplicationController
+  def index
+    @days = Day.all
+  end
+
+  def new
+    @day = Day.new
+  end
+
+  def create
+    Day.create(day_params)
+
+    redirect_to action: :index
+  end
+
+  def destroy
+    Day.find(params[:id]).destroy
+
+    redirect_to action: :index
+  end
+
+  def edit
+    @day = Day.find(params[:id])
+  end
+
+  def update
+    Day.find(params[:id]).update(day_params)
+
+    redirect_to action: :index
+  end
+
+  def show
+    @day = Day.find(params[:id])
+  end
+
+  private
+
+  def day_params
+    params.require(:day).permit(:date, :instructor, :assistant, :takeoff)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,3 @@
 Rails.application.routes.draw do
-  resources :gliders
-  resources :students
-  resources :days do
-      resources :students, module: :days do
-        resources :flights, only: [:index] do
-          # this is the same as:
-          # get 'days/:id/students/:id/flights', to: 'students#flights', as: 'flights'
-          collection do
-              patch 'update_all'
-              put 'update_all'
-              post 'create_many'
-              delete 'destroy_many'
-          end
-        end
-      end
-      member do
-        post :add_student
-      # resources :flights,
-      end
-  end
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :days
 end
