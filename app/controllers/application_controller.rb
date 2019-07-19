4f802cc21dd62e5d1bb34b6061d67ab15421539e
Hana Harencarova <h.harencarova@gmail.com>
2019-07-19 20:46:43 +0200

    Add users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'skyjam' && password == ENV['SKYJAM_PASSWORD']
+      username == 'skyjam' && password == ENV['SKYJAM_PASSWORD'] ||
+      username == 'ales' && password == ENV['ALES_PASSWORD'] ||
+      username == 'markus' && password == ENV['MARKUS_PASSWORD']
     end
   end
 end
