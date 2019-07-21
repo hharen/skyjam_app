1af5ebf5529590e5d85e178ef223951111fd2199
nerdinand <nerdinand@nerdinand.com>
2019-07-21 11:24:58 +0200

    Change selectable gliders based on selected student

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,26 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+var onStudentSelectChange = function(){
+  var gliderSelect = document.getElementById('attendance_glider_id');
+  var studentId = this.value;
+  var availableGliderOptions = gliderSelect.querySelectorAll('option[data-student-id="' + studentId + '"], option[data-student-id=""]');
+  
+  gliderSelect.querySelectorAll('option').forEach(function(option) {
+    option.style.display = 'none';
+  });
+
+  availableGliderOptions.forEach(function(option) {
+    option.style.display = 'block';
+  });
+
+  gliderSelect.value = availableGliderOptions[0].value;
+};
+
+
+document.addEventListener('turbolinks:load', function() {
+  onStudentSelectChange();
+
+  document.getElementById('attendance_student_id').addEventListener('change', onStudentSelectChange);
+});
