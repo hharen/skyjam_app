2a6977afb5dfb90efe2849959107790ca76dcef4
nerdinand <nerdinand@nerdinand.com>
2019-07-21 21:30:52 +0200

    Make glider colors changeable in glider form

--- app/models/glider.rb
+++ app/models/glider.rb
@@ -1,11 +1,28 @@
 class Glider < ApplicationRecord
+  COLORS = [
+    'black',
+    'silver',
+    'blue',
+    'red',
+    'orange',
+    'gold',
+    'white'
+  ]
+
   belongs_to :student, optional: true
   has_many :attendances
   mount_uploader :image, PictureUploader
-
 
   def name
     "#{glider_type} #{size} | #{manufacturer} | #{color}"
   end
 
+  def background_color
+  end
+
+  def arc_color
+  end
+
+  def oval_color
+  end
 end
