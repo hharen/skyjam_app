35c3426621b409c8084666eee9a85b93e83f2db8
Hana Harencarova <h.harencarova@gmail.com>
2019-07-27 19:36:51 +0200

    Change the glider image size. Add glider image to show and index pages.

--- app/models/glider.rb
+++ app/models/glider.rb
@@ -11,8 +11,8 @@ class Glider < ApplicationRecord
 
   belongs_to :student, optional: true
   has_many :attendances
-  
+
   def name
-    "#{glider_type} #{size} | #{manufacturer} | #{color}"
+    "#{glider_type} #{size} | #{manufacturer}"
   end
 end
