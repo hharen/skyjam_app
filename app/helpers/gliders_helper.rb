f5eef05d63260e6922a800dcfac845906c4a5f0b
Hana Harencarova <h.harencarova@gmail.com>
2019-07-29 18:42:12 +0200

    Change the glider.svg path.

--- app/helpers/gliders_helper.rb
+++ app/helpers/gliders_helper.rb
@@ -1,6 +1,6 @@
 module GlidersHelper
   def show_glider_svg(glider)
-    File.open("app/assets/images/glider.svg.erb", 'rb') do |file|
+    File.open("app/assets/glider.svg.erb", 'rb') do |file|
       ERB.new(file.read).result_with_hash(glider: glider).html_safe
     end
   end
