2c23e762c1ffa659c36da24c61fe47fd16683b82
Hana Harencarova <h.harencarova@gmail.com>
2019-08-02 22:24:44 +0200

    Hide unchecked option when selecting the glider colors

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,8 @@
+require("turbolinks").start()
+
+import 'bootstrap'
+import '../gliders/glider_color_picture'
+import '../gliders/student_select'
+import '../gliders/glider_color_select'
+import '../src/main'
+import '../styles/application.scss'
