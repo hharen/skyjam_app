f6e2ce7047fe77af4b1919f854e3735257cd8e78
Hana Harencarova <h.harencarova@gmail.com>
2020-02-24 20:14:13 +0100

    revert choices.js change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,8 @@
 require("turbolinks").start()
-require("choices.js")
 
 import 'bootstrap'
 import '../gliders/glider_color_picture'
 import '../gliders/student_select'
 import '../gliders/glider_color_select'
-import '../gliders/glider_select'
 import '../src/main'
 import '../styles/application.scss'
