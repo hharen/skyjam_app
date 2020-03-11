e74d08c2265ad173cf8b108c5077f9b6a9f1cb55
Hana Harencarova <h.harencarova@gmail.com>
2020-03-11 18:32:10 +0100

    Add login for rgsoc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::Base
       username == 'skyjam' && password == ENV['SKYJAM_PASSWORD'] ||
       username == 'ales' && password == ENV['ALES_PASSWORD'] ||
       username == 'markus' && password == ENV['MARKUS_PASSWORD'] ||
-      username == 'delf' && password == ENV['DELF_PASSWORD']
+      username == 'delf' && password == ENV['DELF_PASSWORD'] ||
+      username == 'rgsoc' && password == ENV['RGSOC_PASSWORD']
     end
   end
 end
