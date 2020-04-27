b01df7dd6df7c3c40961a4a84b709c36ba12c759
Hana Harencarova <h.harencarova@gmail.com>
2020-04-27 20:31:45 +0200

    Make the tests nicer

--- spec/models/glider_spec.rb
+++ spec/models/glider_spec.rb
@@ -4,22 +4,18 @@ RSpec.describe Glider, type: :model do
   fixtures :all
 
   describe '#name' do
-    context 'glider name' do # how to improve this?
-      subject(:glider) do
-        Glider.new(glider_type: 'Wave 2', size: 'S', manufacturer: 'Skyjam')
-      end
-
-      it 'returns "Wave 2 | Skyjam"' do # should I use '' and "" the other way around?
-        expect(glider.name).to eq('Wave 2 S | Skyjam')
-      end # should I test also something else?
+    subject(:glider) do
+      Glider.new(glider_type: 'Wave 2', size: 'S', manufacturer: 'Skyjam')
     end
+
+    it 'returns "Wave 2 | Skyjam"' do
+      expect(glider.name).to eq('Wave 2 S | Skyjam')
+    end # should I test also something else?
   end
 
   describe '#text_color?' do
     context 'glider has color' do
-      subject(:glider) do
-        Glider.new(color: 'black')
-      end
+      subject(:glider) { Glider.new(color: 'black') }
 
       it 'is true' do
         expect(glider.text_color?).to be true
@@ -27,7 +23,7 @@ RSpec.describe Glider, type: :model do
     end
 
     context 'glider does not have color' do
-      subject(:glider) { Glider.new } # which style is better?
+      subject(:glider) { Glider.new }
 
       it 'is false' do
         expect(glider.text_color?).to be false
