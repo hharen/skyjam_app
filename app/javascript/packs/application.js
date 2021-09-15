429f46538e1f3e0b3f764bc19f14e808581d95f3
Hana Harencarova <h.harencarova@gmail.com>
2021-09-15 18:01:52 +0200

    Fix asset precompilation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,19 @@
+// import 'bootstrap/js/src/alert'  
+// import 'bootstrap/js/src/button'  
+// import 'bootstrap/js/src/carousel'  
+import 'bootstrap/js/src/collapse'  
+import 'bootstrap/js/src/dropdown'  
+// import 'bootstrap/js/src/modal'  
+// import 'bootstrap/js/src/popover'  
+import 'bootstrap/js/src/scrollspy'  
+// import 'bootstrap/js/src/tab'  
+// import 'bootstrap/js/src/toast'  
+// import 'bootstrap/js/src/tooltip'  
+
 require("turbolinks").start()
 
-import 'bootstrap'
 import '../gliders/glider_color_picture'
 import '../gliders/student_select'
 import '../gliders/glider_color_select'
 import '../src/main'
-import '../styles/application.scss'
+// import '../styles/application.scss'
